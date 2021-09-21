fce94cd4fc6a03e5c9ab5939c85edd29a61af99c
Ritesh Sinha <Ritesh.Sinha@mLogica.com>
2021-09-21 10:28:21 +0100

    Hi We have new changes made here Go Ahead and make changes please

--- LearnHinduism.com/Controllers/HomeController.cs
+++ LearnHinduism.com/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace LearnHinduism.com.Controllers
         {
             return View();
         }
+        public ActionResult Gods()
+        {
+            return View();
+        }
     }
 }
